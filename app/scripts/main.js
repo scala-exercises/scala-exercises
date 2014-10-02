36245b30722ad74d62ceeeeb9066ed564c0303a8
Rafa Paradela <rafa.p@47deg.com>
2014-10-02 16:43:21 +0200

    some minor changes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,33 @@
-console.log('\'Allo \'Allo!');
+$(function() {
+    
+    loadStepIconAnimation();
+    loadIconGeneralAnimation();
+});
+
+
+function loadStepIconAnimation() {
+    $('.step-icon').each(function(index) {
+        $(this).waypoint(function() {
+            showAniming($(this), 'fadeInLeft');
+        }, { offset: 'bottom-in-view', triggerOnce: true });
+    });
+}
+
+function loadIconGeneralAnimation() {
+    $('.icon-general').each(function(index) {
+        var delay = index * 800;
+        var item = $(this);
+        item.waypoint(function() {
+            setTimeout(function(){
+                showAniming(item, 'fadeInDown');
+            }, delay);
+        }, { offset: 'bottom-in-view', triggerOnce: true });
+    });
+}
+
+function showAniming(element, animation) {
+    element.css('opacity','1.0').addClass('animated').addClass(animation);
+    setTimeout(function(){
+        element.removeClass('animatedss').removeClass(animation);
+    }, 3000);
+}
