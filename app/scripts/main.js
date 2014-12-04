b309217ce731fc73d8a64e2327f6a2f76db018f2
Rafa Paradela <rafa.p@47deg.com>
2014-12-04 18:17:16 +0100

    Design comments fixed

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,16 @@
 $(function() {
     
-    loadStepIconAnimation();
-    loadIconGeneralAnimation();
+    $.when(
+        $('head').append('<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" type="text/css" />'),
+        $.ready.promise()
+    ).then(function(){
+        loadStepIconAnimation();
+        loadIconGeneralAnimation();
+    });
+    
 });
+
+var base_url = location.protocol + '//' + location.host;
 
 
 function loadStepIconAnimation() {
@@ -35,4 +43,22 @@ function showAniming(element, animation) {
 function moveToDiv(id) {
 	$('html, body').animate({scrollTop:$("#"+id).offset().top}, 500);
 	return false;
+}
+
+function shareSiteFacebook() {
+    var title = '"Doing Scala" offers hundreds of exercises that covering main concepts of Scala!';
+    launchPopup('http://www.facebook.com/sharer/sharer.php?u='+base_url+'&t='+title)
+}
+
+function shareSiteTwitter() {
+    var text = '"Doing Scala" offers hundreds of exercises that covering main concepts of Scala! '+base_url;
+    launchPopup('https://twitter.com/home?status='+text)
+}
+
+function shareSiteGoogle() {
+    launchPopup('https://plus.google.com/share?url='+base_url)
+}
+
+function launchPopup(url) {
+    window.open(url, 'Social Share', 'height=320, width=640, toolbar=no, menubar=no, scrollbars=no, resizable=no, location=no, directories=no, status=no');
 }
