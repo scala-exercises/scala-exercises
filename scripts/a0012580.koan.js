79a6f31a3f30a250496a41e633e77d2d380c8078
Justin Elsberry <justin@47deg.com>
2014-12-05 13:55:48 -0800

    Built scala-koans-online from commit 9300b38 on branch master

--- scripts/a0012580.koan.js
+++ scripts/a0012580.koan.js
@@ -1,5 +1,5 @@
 $(function() {
-    
+
     $.when(
 		$.getScript('http://yandex.st/highlightjs/8.0/highlight.min.js'),
 		$.getScript('http://cdnjs.cloudflare.com/ajax/libs/marked/0.3.2/marked.min.js'),
@@ -8,11 +8,11 @@ $(function() {
         loadDisqus(),
         $.ready.promise()
     ).then(function(){
-		
+
         loadNav();
         init();
         activeMenu();
-        
+
         // colourify();
 		// buscaReplace();
     });
@@ -55,7 +55,7 @@ function init() {
         writeCurrent(hash);
         readKoanJSON();
     }
-    
+
 }
 
 // window.location.hash = tb;
@@ -67,12 +67,12 @@ function drawNavItem(index, kg) {
     var item = $('<li></li>').attr({'class':'item', 'data-id':getId(kg)}); $('#sidebar_menu').append(item);
     var a = $('<a></a>').attr({'href':'#', 'data-id':getId(kg)}).text(kg); item.append(a);
     var i = $('<i></i>').attr({'class':'pull-right fa'}); a.prepend(i);
-    
+
     a.click(function(event){
         event.preventDefault();
         window.location.hash = $(this).attr('data-id');
     });
-    
+
 }
 
 function restoreNav() {
@@ -91,7 +91,7 @@ function restoreNav() {
         dev = true;
     }
     return dev;
-    
+
 }
 
 function setInitialKoan() {
@@ -117,18 +117,18 @@ function updateCompletes() {
 function readKoanJSON() {
     runReadingMode();
     var url = "json/"+hash+".json";
-    
+
     $.getJSON(url, function(data) {
         hideReadingMode(data);
-        
+
     })
     .error(function (xhr, ajaxOptions, thrownError){
         if(xhr.status==404) {
-            
+
             console.log("JSON not found");
         }
     });
-    
+
 }
 
 function drawKoan() {
@@ -138,7 +138,7 @@ function drawKoan() {
     // var container = $('<div></div>').attr({'class':'container'}); item.append(container);
 	var h2 = $('<h2></h2>').text(koan.title); item.append(h2);
 	var content = $('<div></div>').attr({'class':'content'}); item.append(content);
-	
+
 	if(koan.modules){
 		$.each(koan.modules, function(index_module, module) {
 			drawModule(content, index_module, module);
@@ -147,11 +147,11 @@ function drawKoan() {
 	}
     colourify();
 	searchInputs();
-    
+
     setTimeout(function(){
       activeInputs();
     }, 500);
-    
+
     reloadComments();
 }
 
@@ -171,15 +171,15 @@ function drawEditLink(content) {
 	var callout = $('<div></div>').attr({'id': 'editLink', 'class':'bs-callout'}).hide(); content.append(callout);
     var h4 = $('<h4></h4>').text('Add exercises'); callout.append(h4);
     var id = getId(koan.title);
-    var p = $('<p></p>').html('If you would like add another interesting exercises about "'+koan.title+'", feel free to edit "'+id+'.json" and submit a pull request: <a target="_blank" href="https://github.com/47deg/doing-scala/edit/master/app/json/'+id+'.json"><i class="fa fa-pencil"></i> Edit</a>'); callout.append(p);
-    
+    var p = $('<p></p>').html('If you would like add other interesting exercises about "'+koan.title+'", feel free to edit "'+id+'.json" and submit a pull request: <a target="_blank" href="https://github.com/47deg/doing-scala/edit/master/app/json/'+id+'.json"><i class="fa fa-pencil"></i> Edit</a>'); callout.append(p);
+
 }
 
 
 
 // Sintaxis
 function colourify() {
-	hljs.initHighlightingOnLoad();	
+	hljs.initHighlightingOnLoad();
 	$('pre code').each(function(i, block) {
         $(block).addClass('scala');
 		hljs.highlightBlock(block);
@@ -188,27 +188,27 @@ function colourify() {
 
 function searchInputs() {
 	$('#module_content pre code').each(function(i, block) {
-		
+
         var code = $(block);
         var module_index=code.attr('data-index');
 	    var texto = code.text();
 		var reemplazado = texto.replace(/\__/g, '<input data-module-index="'+module_index+'" type="text" value="" />');
-	    code.html(reemplazado);	
+	    code.html(reemplazado);
         hljs.highlightBlock(block);
-        
+
 	});
-	
+
 }
 
 function activeInputs() {
-    
+
     $('#module_content .module').each(function(module_index) {
         var module = $(this);
         var codes = module.find('pre code');
         module.find('pre code').each(function(code_index) {
             var code = $(this);
-            
-            
+
+
             var pre = code.parent();
             var inputs = code.find('input');
             if(inputs.length>0){
@@ -220,7 +220,7 @@ function activeInputs() {
                     var inputSize = calculeSize(moduleIndex, input_index);
                     // input.width(inputSize);
                     input.animate({width: inputSize+"px"}, 300 );
-                
+
                     var answer = readSolution(hash,moduleIndex,input_index);
                     if(answer.length>0){
                         input.val(answer);
@@ -241,14 +241,14 @@ function activeInputs() {
 
             	});
             }
-            
+
         });
-        
-        
+
+
     });
-    
+
     editing = true;
-    
+
 }
 
 function calculeSize(moduleIndex, index) {
@@ -270,7 +270,7 @@ function mySize(length) {
 function studySolution(element) {
     var input = $(element);
     var answer = input.val();
-    
+
     var size = mySize(answer.length);
     console.log(size);
     input.animate({width: size+"px"}, 100 );
@@ -318,7 +318,7 @@ function studyKoanStatus() {
         else writeKoanComplete(hash);
     }
     updateCompletes();
-    
+
 }
 
 
@@ -330,14 +330,14 @@ function drawSources(data) {
     wrapper.empty();
 	var item = $('<div></div>').attr({'class':'item'}); wrapper.append(item);
 	var h2 = $('<h2></h2>').text(data.title); item.append(h2);
-	var p = $('<p></p>').text('Modules have been written after following several Scala documentation sites. All of them constitute an interesting stack of awesome links, listed below.'); item.append(p);
+	var p = $('<p></p>').text('Modules were written from several Scala documentation sites. All of them contain an interesting stack of awesome links, listed below.'); item.append(p);
 	var content = $('<ul></ul>').attr({'class':'content'}); item.append(content);
-    
+
 	var callout = $('<div></div>').attr({'class':'bs-callout'}); item.append(callout);
     var h4 = $('<h4></h4>').text('Add links'); callout.append(h4);
-    var p = $('<p></p>').html('If you would like add another interesting link about Scala, feel free to edit the "sources.json" and pull request:  <a target="_blank" href="https://github.com/47deg/doing-scala/edit/master/app/json/sources.json"><i class="fa fa-pencil"></i> Edit</a>'); callout.append(p);
-	
-	
+    var p = $('<p></p>').html('If you would like add other interesting links about Scala, feel free to edit the "sources.json" and pull request:  <a target="_blank" href="https://github.com/47deg/doing-scala/edit/master/app/json/sources.json"><i class="fa fa-pencil"></i> Edit</a>'); callout.append(p);
+
+
     if(data.sources){
         $.each(data.sources, function(index_source, source) {
             drawSource(content, index_source, source);
@@ -424,7 +424,7 @@ function goToNext() {
     var num_items = items.length;
     var index = current.index();
     var next = index+1;
-    
+
     if(next<num_items){
         var next_item = items.get(next);
         var next_link = $(next_item).find('a');
@@ -461,12 +461,12 @@ function shareStepGoogle() {
 }
 
 function shareCourseFacebook() {
-    var title = "I've just completed all Scala exercises!";
+    var title = "I've just completed all the Scala exercises!";
     launchPopup('http://www.facebook.com/sharer/sharer.php?u='+base_url+'&t='+title)
 }
 
 function shareCourseTwitter() {
-    var text = "I've just completed all Scala exercises at "+base_url;
+    var text = "I've just completed all the Scala exercises at "+base_url;
     launchPopup('https://twitter.com/home?status='+text)
 }
 
@@ -489,18 +489,18 @@ function runReadingMode() {
 
 function hideReadingMode(data) {
     setTimeout(function(){
-        
+
         if(hash == 'sources') drawSources(data)
         else{
             koan = data;
             drawKoan();
         }
-        
+
         setTimeout(function(){
             $('#waiting').addClass('almosthidden');
         }, 500);
     }, 500);
-    
+
 }
 
 
@@ -509,15 +509,15 @@ function hideReadingMode(data) {
 function loadDisqus() {
     var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
     dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);    
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
 }
 
 
 function reloadComments() {
-    
-    
-    
-    
+
+
+
+
     if(typeof DISQUS === 'undefined'){
         console.log('Disqus not defined');
 
@@ -525,13 +525,13 @@ function reloadComments() {
         var discussion_url = base_url + '/#!' + hash;
         DISQUS.reset({
           reload: true,
-          config: function () {  
+          config: function () {
               this.page.identifier = hash;
               this.page.url = discussion_url;
           }
         });
     }
-    
+
 }
 
 //Console
@@ -547,7 +547,7 @@ function activeMenu() {
         e.preventDefault();
         $("#sidebar-wrapper").toggleClass("active");
     });
-	
+
     $("#menu-toggle").click(function(e) {
         e.preventDefault();
         $("#sidebar-wrapper").toggleClass("active");
@@ -576,4 +576,3 @@ function moveToTop() {
 	$('html, body').animate({scrollTop:0}, 500);
 	return false;
 }
-
