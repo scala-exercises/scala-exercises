f88784ef9c6c3a2c2c8c8d063c85ced6ae1c1961
Rafa Paradela <rafa.p@47deg.com>
2015-01-15 13:03:47 +0100

    Again deployment bash file

--- deployIt.sh
+++ deployIt.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-COMMIT_MESSAGE = git log --oneline -1 | cut -d " " -f2-
+
+COMMIT_MESSAGE="$(git log --oneline -1 | cut -d " " -f2-)"
 grunt build
-git add dist && git commit -m "DISTRIBUTION $COMMIT_MESSAGE" 
+git add dist && git commit -m "Distribution: $COMMIT_MESSAGE"
 git push origin master
 grunt buildcontrol:pages
