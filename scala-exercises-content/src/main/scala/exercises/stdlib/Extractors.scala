42492016f8731bfa4b00198967c4e90d269338e2
Raúl Raja Martínez <raulraja@gmail.com>
2015-10-06 17:05:59 +0200

    Exercise parser and evaluator

--- scala-exercises-content/src/main/scala/exercises/stdlib/Extractors.scala
+++ scala-exercises-content/src/main/scala/exercises/stdlib/Extractors.scala
@@ -1,6 +1,8 @@
-package shared
+package exercises.stdlib
 
 import org.scalatest._
+import shared.ExerciseRunner.{ExerciseResult, ∞}
+import shared.{ExerciseRunner, Exercises}
 
 /**
  *
@@ -10,45 +12,47 @@ import org.scalatest._
  *
  *
  * object Twice {
- *   def apply(x: Int): Int = x * 2
- *   def unapply(z: Int): Option[Int] = if (z%2 == 0) Some(z/2) else None
+ * def apply(x: Int): Int = x * 2
+ * def unapply(z: Int): Option[Int] = if (z%2 == 0) Some(z/2) else None
  * }
  *
  * object TwiceTest extends Application {
- *   val x = Twice(21)
- *   x match { case Twice(n) => Console.println(n) } // prints 21
+ * val x = Twice(21)
+ * x match { case Twice(n) => Console.println(n) } // prints 21
  * }
  *
  *
  * There are two syntactic conventions at work here:
  *
- *  * The pattern `case Twice(n)` will cause an invocation of `Twice.unapply`, which is used to match even number; the return value of the `unapply` signals whether the argument has matched or not, and any sub-values that can be used for further matching. Here, the sub-value is `z/2`
- *  * The `apply` method is not necessary for pattern matching. It is only used to mimick a constructor. `val x = Twice(21)` expands to `val x = Twice.apply(21)`.
+ * * The pattern `case Twice(n)` will cause an invocation of `Twice.unapply`, which is used to match even number; the return value of the `unapply` signals whether the argument has matched or not, and any sub-values that can be used for further matching. Here, the sub-value is `z/2`
+ * * The `apply` method is not necessary for pattern matching. It is only used to mimick a constructor. `val x = Twice(21)` expands to `val x = Twice.apply(21)`.
  *
  * The code in the preceding example would be expanded as follows:
  *
  *
  * object TwiceTest extends Application {
- *   val x = Twice.apply(21)
- *   Twice.unapply(x) match { case Some(n) => Console.println(n) } // prints 21
+ * val x = Twice.apply(21)
+ * Twice.unapply(x) match { case Some(n) => Console.println(n) } // prints 21
  * }
  *
  * The return type of an `unapply` should be chosen as follows:
  *
- *  * If it is just a test, return a `Boolean`. For instance `case even()`
- *  * If it returns a single sub-value of type `T`, return a `Option[T]`
- *  * If you want to return several sub-values `T1,...,Tn`, group them in an optional tuple `Option[(T1,...,Tn)]`.
+ * * If it is just a test, return a `Boolean`. For instance `case even()`
+ * * If it returns a single sub-value of type `T`, return a `Option[T]`
+ * * If you want to return several sub-values `T1,...,Tn`, group them in an optional tuple `Option[(T1,...,Tn)]`.
  *
  * Sometimes, the number of sub-values is fixed and we would like to return a sequence. For this reason, you can also define patterns through `unapplySeq`. The last sub-value type `Tn` has to be `Seq[S]`. This mechanism is used for instance in pattern `case List(x1, ..., xn)`.
  *
  */
-class Extractors extends FlatSpec with Matchers {
+class Extractors extends FlatSpec with Matchers with Exercises {
 
 
   /**
    * When you create a case class, it automatically can be used with pattern matching since it has an extractor:
    */
-  def extractorCaseClasses(solutions: Seq[Any]): Unit = {
+  def caseClasses(res0: String): ExerciseResult[Unit] = ExerciseRunner("Case Classes") {
+
+    println("Inside case classes args : " + res0)
 
     case class Employee(firstName: String, lastName: String)
 
@@ -58,15 +62,18 @@ class Extractors extends FlatSpec with Matchers {
       case _ => "No Batman Joke For You"
     }
 
-    result should be(solutions(0))
-  }
+    result should be(res0)
+
+    println("after assert")
+
+  }(∞)
 
 
   /**
    * What's an extractor? In Scala it's a method in any `object` called `unapply`, and that method is used to disassemble the object given by returning a tuple wrapped in an option.
    * Extractors can be used to assign values:
    */
-  def extractorForAssigningValues(solutions: Seq[Any]): Unit = {
+  def forAssigningValues(res0: String, res1: String, res2: Int, res3: Int): ExerciseResult[Unit] = ExerciseRunner("For Assigning Values") {
 
     class Car(val make: String, val model: String, val year: Short, val topSpeed: Short)
 
@@ -76,17 +83,17 @@ class Extractors extends FlatSpec with Matchers {
 
     val ChopShop(a, b, c, d) = new Car("Chevy", "Camaro", 1978, 120)
 
-    a should be(solutions(0))
-    b should be(solutions(1))
-    c should be(solutions(2))
-    d should be(solutions(3))
-  }
+    a should be(res0)
+    b should be(res1)
+    c should be(res2)
+    d should be(res3)
+  }(∞)
 
 
   /**
    * Of course an extractor can be used in pattern matching...
    */
-  def extractorInPatternMatching(solutions: Seq[Any]): Unit = {
+  def patternMatching(res0: String, res1: String): ExerciseResult[Unit] = ExerciseRunner("Pattern Matching") {
 
     class Car(val make: String, val model: String, val year: Short, val topSpeed: Short)
 
@@ -99,14 +106,14 @@ class Extractors extends FlatSpec with Matchers {
       case _ => ("Ford", "Edsel")
     }
 
-    x._1 should be(solutions(0))
-    x._2 should be(solutions(1))
-  }
+    x._1 should be(res0)
+    x._2 should be(res1)
+  }(∞)
 
   /**
    * Since we aren't really using u and v in the previous pattern matching with can replace them with _.
    */
-  def extractorInPatternMatchingWithWildcards(solutions: Seq[Any]): Unit = {
+  def inPatternMatchingWithWildcards(res0: String, res1: String): ExerciseResult[Unit] = ExerciseRunner("Pattern Matching Wildcards") {
 
     class Car(val make: String, val model: String, val year: Short, val topSpeed: Short)
 
@@ -120,15 +127,15 @@ class Extractors extends FlatSpec with Matchers {
       case _ => ("Ford", "Edsel")
     }
 
-    x._1 should be(solutions(0))
-    x._2 should be(solutions(1))
-  }
+    x._1 should be(res0)
+    x._2 should be(res1)
+  }(∞)
 
 
   /**
    * As long as the method signatures aren't the same, you can have as many unapply methods as you want:
    */
-  def extractorUnapplyDifferentSignature(solutions: Seq[Any]): Unit = {
+  def unapplyDifferentSignature(res0: String): ExerciseResult[Unit] = ExerciseRunner("Unnaply Different Signatures") {
 
     class Car(val make: String, val model: String, val year: Short, val topSpeed: Short)
     class Employee(val firstName: String, val middleName: Option[String], val lastName: String)
@@ -144,13 +151,13 @@ class Extractors extends FlatSpec with Matchers {
       case _ => "Not found"
     }
 
-    result should be(solutions(0))
-  }
+    result should be(res0)
+  }(∞)
 
   /**
    * An extractor can be any stable object, including instantiated classes with an unapply method.
    */
-  def extractorClassesWithUnapply(solutions: Seq[Any]): Unit = {
+  def classesWithUnapply(res0: String): ExerciseResult[Unit] = ExerciseRunner("Classes with Unapply") {
 
     class Car(val make: String, val model: String, val year: Short, val topSpeed: Short) {
       def unapply(x: Car) = Some(x.make, x.model)
@@ -163,14 +170,14 @@ class Extractors extends FlatSpec with Matchers {
       case _ => "unknown"
     }
 
-    result should be(solutions(0))
-  }
+    result should be(res0)
+  }(∞)
 
   /**
    * What is typical is to create a custom extractor in the companion object of the class.
    * In this exercise, we use it as an assignment:
    */
-  def extractorInCompanionObject(solutions: Seq[Any]): Unit = {
+  def inCompanionObject(res0: String, res1: Option[String], res2: String): ExerciseResult[Unit] = ExerciseRunner("In companion Object") {
 
     class Employee(val firstName: String,
         val middleName: Option[String],
@@ -187,15 +194,15 @@ class Extractors extends FlatSpec with Matchers {
 
     val Employee(a, b, c) = singri
 
-    a should be(solutions(0))
-    b should be(solutions(1))
-    c should be(solutions(2))
-  }
+    a should be(res0)
+    b should be(res1)
+    c should be(res2)
+  }(∞)
 
   /**
    * In this exercise we use the unapply for pattern matching employee objects
    */
-  def extractorUnapplyForPatternMatching(solutions: Seq[Any]): Unit = {
+  def unapplyForPatternMatching(res0: String): ExerciseResult[Unit] = ExerciseRunner("Unapply for pattern matching") {
 
     class Employee(val firstName: String,
         val middleName: Option[String],
@@ -216,9 +223,9 @@ class Extractors extends FlatSpec with Matchers {
       case _ => "I don't care, going on break"
     }
 
-    result should be(solutions(0))
-  }
-
+    result should be(res0)
+  }(∞)
 
 
 }
+
---
+++ scala-exercises-server/test/ExercisesServiceSpec.scala
@@ -0,0 +1,57 @@
+import models.ExerciseEvaluation
+import org.junit.runner._
+import org.specs2.mutable._
+import org.specs2.runner._
+import org.specs2.scalaz.DisjunctionMatchers
+import services.parser.ExercisesService
+
+
+@RunWith(classOf[JUnitRunner])
+class ExercisesServiceSpec extends Specification with DisjunctionMatchers {
+
+  val expectedTestSection = "stdlib"
+  val expectedTestCategory = "Extractors"
+  val expectedTestExercise = "forAssigningValues"
+  val expectedTestSuccesArgs = List("Chevy", "Camaro", "1978", "120")
+  val expectedTestFailedArgs = List("a", "b", "1", "2")
+
+  "ExercisesService" should {
+
+    "return at least one section via classpath discovery" in {
+      val sections = ExercisesService.sections
+      sections must not be empty
+      sections.find(_.title == expectedTestSection) must beSome
+    }
+
+    "return at least one category via classpath discovery" in {
+      val foundCategories = for {
+        section <- ExercisesService.sections
+        categoryS <- section.categories
+        category <- ExercisesService.category(section.title, categoryS)
+      } yield category
+      foundCategories must not be empty
+      val expectedCat = foundCategories.find(_.title == expectedTestCategory)
+      expectedCat must beSome
+      val category = expectedCat.get
+      category.exercises must not be empty
+      category.exercises.find(_.method.contains(expectedTestExercise)) must beSome
+    }
+
+    "evaluate a known exercise type coercing it's parameters and get a successful result" in {
+      ExercisesService.evaluate(ExerciseEvaluation(
+        section = expectedTestSection,
+        category = expectedTestCategory,
+        method = expectedTestExercise,
+        args = expectedTestSuccesArgs)) must beRightDisjunction
+    }
+
+    "evaluate a known exercise type coercing it's parameters and get a failed result" in {
+      ExercisesService.evaluate(ExerciseEvaluation(
+        section = expectedTestSection,
+        category = expectedTestCategory,
+        method = expectedTestExercise,
+        args = expectedTestFailedArgs)) must beLeftDisjunction
+    }
+
+  }
+}
