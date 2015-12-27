9ddcd88e74ffe0db7d752c61777fb3022464d648
Rafa Paradela <rafa.p@47deg.com>
2015-12-27 23:34:48 +0100

    Added "color" to Section model and defined the color into the stdlib section

--- scala-exercises-content/src/main/scala/exercises/stdlib/package.scala
+++ scala-exercises-content/src/main/scala/exercises/stdlib/package.scala
@@ -4,6 +4,7 @@ import shared.SectionPkg
 
 /** Main Title and description for Standard Lib exercises goes here.
   * This text is just a placeholder that should be replaced by the content author.
+  * Color:#0f7e8c
   */
 package object stdlib extends SectionPkg
 
