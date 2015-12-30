999c23a2c200bb5907ed144e71e5b5ce33578ea6
Rafa Paradela <rafa.p@47deg.com>
2015-12-30 01:36:12 +0100

    Added Highlight.js for code colouring

---
+++ scala-exercises-client/src/main/scala/example/ExercisesJS.scala
@@ -0,0 +1,17 @@
+package example
+
+import scala.scalajs.js
+import org.scalajs.dom
+import org.scalajs.jquery.{ jQuery ⇒ $ }
+
+object ExercisesJS extends js.JSApp {
+
+  def main(): Unit = {
+    runHighlight
+  }
+
+  def runHighlight = $("pre code").each({ (f: Any, code: dom.Element) ⇒
+    js.Dynamic.global.hljs.highlightBlock(code)
+  })
+
+}
