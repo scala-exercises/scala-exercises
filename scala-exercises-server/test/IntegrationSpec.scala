9d3ed4bc159571c597059bc30cdae91babd51f09
Rafa Paradela <rafa.p@47deg.com>
2015-12-30 16:48:53 +0100

    Added TestServer instead of webBrowser

--- scala-exercises-server/test/IntegrationSpec.scala
+++ scala-exercises-server/test/IntegrationSpec.scala
@@ -1,25 +1,18 @@
-import org.openqa.selenium.htmlunit.HtmlUnitDriver
 import org.specs2.mutable._
-import org.specs2.runner._
-import org.junit.runner._
 import play.api.test._
+import play.api.test.Helpers._
 
-/**
- * add your integration spec here.
- * An integration test will fire up a whole play application in a real (or headless) browser
- */
-@RunWith(classOf[JUnitRunner])
 class IntegrationSpec extends Specification {
 
-  "Application" should {
+  "Home page" should {
 
-    "work from within a browser" in new WithBrowser {
-
-      val htmlunitDriver = webDriver.asInstanceOf[HtmlUnitDriver]
-      htmlunitDriver.setJavascriptEnabled(false)
-      browser.goTo("http://localhost:" + port)
-      browser.pageSource must contain("Scala Exercises")
-
+    "should contain Scala Exercise string" in {
+      running(TestServer(3333), HTMLUNIT) { browser =>
+        browser.goTo("http://localhost:3333/")
+        browser.pageSource must contain("Scala Exercises")
+      }
     }
+
   }
+
 }
