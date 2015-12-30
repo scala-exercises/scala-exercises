408f855eb6ee9bcdc071d78b5b12b867825e40d8
Rafa Paradela <rafa.p@47deg.com>
2015-12-30 17:09:07 +0100

    Hidden the exercises service tests

--- scala-exercises-server/test/IntegrationSpec.scala
+++ scala-exercises-server/test/IntegrationSpec.scala
@@ -1,18 +1,18 @@
-//import org.specs2.mutable._
-//import play.api.test._
-//import play.api.test.Helpers._
-//
-//class IntegrationSpec extends Specification {
-//
-//  "Home page" should {
-//
-//    "should contain Scala Exercise string" in {
-//      running(TestServer(3333), HTMLUNIT) { browser =>
-//        browser.goTo("http://localhost:3333/")
-//        browser.pageSource must contain("Scala Exercises")
-//      }
-//    }
-//
-//  }
-//
-//}
+import org.specs2.mutable._
+import play.api.test._
+import play.api.test.Helpers._
+
+class IntegrationSpec extends Specification {
+
+  "Home page" should {
+
+    "should contain Scala Exercise string" in {
+      running(TestServer(3333), HTMLUNIT) { browser =>
+        browser.goTo("http://localhost:3333/")
+        browser.pageSource must contain("Scala Exercises")
+      }
+    }
+
+  }
+
+}
