d0730f3ddd6a2a685a2e59b73c96221cf68fc90d
Rafa Paradela <rafa.p@47deg.com>
2016-01-11 10:53:05 +0100

    Moved exercise model to shared moduled, in order to be used by client

--- scala-exercises-shared/src/main/scala/shared/Category.scala
+++
@@ -1,8 +0,0 @@
-package shared
-
-/** A category. For example `Extractors`
-  */
-case class Category(
-    title: String,
-    description: Option[String] = None,
-    exercises: List[Exercise] = Nil)
