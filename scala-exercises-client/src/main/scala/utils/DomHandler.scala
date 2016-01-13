889dc5ab5b4035e8e11133a6243af46dfa5299b7
Rafa Paradela <rafa.p@47deg.com>
2016-01-13 17:06:51 +0100

    Removed ugly println

--- scala-exercises-client/src/main/scala/utils/DomHandler.scala
+++ scala-exercises-client/src/main/scala/utils/DomHandler.scala
@@ -38,7 +38,6 @@ object DomHandler {
     */
   def onInputBlur(onBlur: String ⇒ IO[Unit]): IO[Unit] = io {
     allInputs foreach (input ⇒ {
-      println("1")
       $(input).blur((e: dom.Event) ⇒ {
         methodParent(input) foreach (onBlur(_).unsafePerformIO())
       })
