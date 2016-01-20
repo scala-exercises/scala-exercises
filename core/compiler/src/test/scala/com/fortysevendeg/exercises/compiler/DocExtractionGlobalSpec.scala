cb144014d506770212d194763b019a705560efcc
Andy Scott <andy.g.scott@gmail.com>
2016-01-20 12:00:36 -0800

    Rework compiler; remove comment cleanup test code until it is fixed

---
+++ core/compiler/src/test/scala/com/fortysevendeg/exercises/compiler/DocExtractionGlobalSpec.scala
@@ -0,0 +1,45 @@
+package com.fortysevendeg.exercises
+package compiler
+
+import org.scalatest._
+import scala.reflect.internal.util.BatchSourceFile
+import scala.util.Try
+
+import scala.tools.nsc._
+import scala.tools.nsc.doc.{ Settings ⇒ _, _ }
+
+class DocExtractionGlobalSpec extends FunSpec with Matchers {
+
+  describe("doc comment searching") {
+    it("should find doc comments on objects") {
+
+      val code = """
+        /** This is a comment that gets ignored */
+        package myPackage {
+          /** This is Foo */
+          object Foo { val bar = 1 }
+          /** This is Bar */
+          object Bar
+        }
+        """
+
+      val g = new DocExtractionGlobal()
+
+      new g.Run() compileSources List(
+        new BatchSourceFile("newSource", code)
+      )
+
+      val currentRun = g.currentRun
+      val rootTree = currentRun.units.toList.head.body
+
+      val res = DocCommentFinder.findByName(g)(rootTree, "myPackage.Foo" :: "myPackage.Bar" :: Nil)
+        .mapValues(_.raw)
+
+      res should equal(Map(
+        "myPackage.Foo" → "/** This is Foo */",
+        "myPackage.Bar" → "/** This is Bar */"
+      ))
+
+    }
+  }
+}
