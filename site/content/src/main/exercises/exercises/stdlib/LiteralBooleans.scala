765c02063c5e63fea0065fdb84eb167044b4f288
Rafa Paradela <rafa.p@47deg.com>
2016-01-22 11:09:16 +0100

    Restored params for LiteralBooleans exercises

--- site/content/src/main/exercises/exercises/stdlib/LiteralBooleans.scala
+++ site/content/src/main/exercises/exercises/stdlib/LiteralBooleans.scala
@@ -8,7 +8,7 @@ class LiteralBooleans extends FlatSpec with Matchers with exercise.Section {
 
   /** Boolean literals are either true or false, using the true or false keyword
     */
-  def literalBooleans(res0: Boolean, res1: Boolean): ExerciseResult[Unit] = ExerciseRunner("Literal Booleans") {
+  def literalBooleans(res0: Boolean, res1: Boolean, res2: Boolean, res3: Boolean, res4: Boolean, res5: Boolean): ExerciseResult[Unit] = ExerciseRunner("Literal Booleans") {
     val a = true
     val b = false
     val c = 1 > 2
@@ -17,6 +17,10 @@ class LiteralBooleans extends FlatSpec with Matchers with exercise.Section {
     val f = b == d
     a should be(res0)
     b should be(res1)
+    c should be(res2)
+    d should be(res3)
+    e should be(res4)
+    f should be(res5)
 
   }(∞)
 
