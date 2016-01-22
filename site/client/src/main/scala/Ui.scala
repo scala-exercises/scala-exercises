b4d06fd00ed71ce6f23d7cbae7457cc442009e6f
Rafa Paradela <rafa.p@47deg.com>
2016-01-22 02:59:31 +0100

    Added an element to contain the response of compilation

--- site/client/src/main/scala/Ui.scala
+++ site/client/src/main/scala/Ui.scala
@@ -5,54 +5,49 @@ import IO._
 import actions._
 import model._
 import model.Exercises._
-import utils.{ DomHandler }
+import utils.DomHandler._
 
 object UI {
   def noop: IO[Unit] = io {}
 
   def update(s: State, a: Action): IO[Unit] = a match {
-    case UpdateExercise(method, args) ⇒ toggleCompileButton(s, method)
+    case UpdateExercise(method, args) ⇒ toggleExerciseClass(s, method)
     case CompileExercise(method)      ⇒ startCompilation(s, method)
     case CompilationOk(method)        ⇒ setAsSolved(s, method)
     case CompilationFail(method)      ⇒ setAsErrored(s, method)
     case _                            ⇒ noop
   }
 
-  def toggleCompileButton(s: State, method: String): IO[Unit] = {
+  def toggleExerciseClass(s: State, method: String): IO[Unit] = {
     Exercises.findByMethod(s, method) match {
       case Some(exercise) ⇒ {
         if (exercise.isFilled)
-          enableCompileButton(method)
+          setClassToExercise(method, "filled")
         else
-          disableCompileButton(method)
+          setClassToExercise(method, "unsolved")
       }
       case _ ⇒ noop
     }
   }
 
-  def enableCompileButton(method: String): IO[Unit] = {
-    DomHandler.nodeByMethod(method)
-      .map(s ⇒ s.foreach(
-        (node ⇒ DomHandler.enableCompileButton(node).unsafePerformIO())
-      ))
-  }
+  def setClassToExercise(method: String, style: String): IO[Unit] = findExerciseByMethod(method)
+    .map(_.foreach(setClass(_, style).unsafePerformIO()))
 
-  def disableCompileButton(method: String): IO[Unit] = {
-    DomHandler.nodeByMethod(method)
-      .map(s ⇒ s.foreach(
-        (node ⇒ DomHandler.disableCompileButton(node).unsafePerformIO())
-      ))
-  }
+  def addLogToExercise(method: String, msg: String): IO[Unit] = findExerciseByMethod(method).map(_.foreach(writeLog(_, msg).unsafePerformIO()))
 
-  def startCompilation(s: State, method: String): IO[Unit] = disableCompileButton(method)
+  def startCompilation(s: State, method: String): IO[Unit] = findByMethod(s, method) match {
+    case Some(exercise) if exercise.isFilled ⇒ setClassToExercise(method, "evaluating")
+    case _                                   ⇒ noop
+  }
 
   def setAsSolved(s: State, method: String): IO[Unit] = for {
-    _ ← disableCompileButton(method)
+    _ ← setClassToExercise(method, "solved")
     _ = println(s"EXERCISE SOLVED $method")
   } yield ()
 
   def setAsErrored(s: State, method: String): IO[Unit] = for {
-    _ ← enableCompileButton(method)
+    _ ← addLogToExercise(method, "Failed compilation")
+    _ ← setClassToExercise(method, "errored")
     _ = println(s"EXERCISE ERRORED $method")
   } yield ()
 }
--- site/client/src/main/scala/scripts/ExercisesJS.scala
+++ site/client/src/main/scala/scripts/ExercisesJS.scala
@@ -4,7 +4,6 @@ import rx._
 import utils.DomHandler._
 
 import scala.scalajs.js
-import scala.concurrent.{ Future }
 import scala.concurrent.ExecutionContext.Implicits.global
 
 import shared.IO
@@ -12,9 +11,9 @@ import IO._
 import model._
 import model.Exercises._
 import actions._
-import ui.{ UI }
-import state.{ State }
-import effects.{ Effects }
+import ui.UI
+import state.State
+import effects.Effects
 
 object ExercisesJS extends js.JSApp {
 
@@ -54,6 +53,8 @@ object ExercisesJS extends js.JSApp {
       _ ← replaceInputs(replacements)
       _ ← onInputKeyUp((method: String, arguments: Seq[String]) ⇒ {
         triggerAction(UpdateExercise(method, arguments))
+      }, (method: String) ⇒ {
+        triggerAction(CompileExercise(method))
       })
       _ ← onButtonClick((method: String) ⇒ {
         triggerAction(CompileExercise(method))
