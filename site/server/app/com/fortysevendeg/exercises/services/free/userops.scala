fcae64aa1f5733ea905558c7cec681fddf725275
Raúl Raja Martínez <raulraja@gmail.com>
2016-01-25 12:55:35 +0100

    Fixed copy and paste comments

--- site/server/app/com/fortysevendeg/exercises/services/free/userops.scala
+++ site/server/app/com/fortysevendeg/exercises/services/free/userops.scala
@@ -13,14 +13,14 @@ import shared.Section
   */
 sealed trait UserOp[A]
 
-/** Exposes Exercise operations as a Free monadic algebra that may be combined with other Algebras via
+/** Exposes User operations as a Free monadic algebra that may be combined with other Algebras via
   * Coproduct
   */
 class UserOps[F[_]](implicit I: Inject[UserOp, F]) {
 
 }
 
-/** Default implicit based DI factory from which instances of the ExerciseOps may be obtained
+/** Default implicit based DI factory from which instances of the UserOps may be obtained
   */
 object UserOps {
 
