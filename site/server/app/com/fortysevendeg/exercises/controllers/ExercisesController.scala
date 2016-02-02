d8eec430c650c7f5e96ad81e00f22a627cce795c
Alejandro Gómez <alejandro@dialelo.com>
2016-02-02 17:52:10 +0100

    Use free algebras from controllers

--- site/server/app/com/fortysevendeg/exercises/controllers/ExercisesController.scala
+++ site/server/app/com/fortysevendeg/exercises/controllers/ExercisesController.scala
@@ -6,18 +6,22 @@ import play.api.libs.json.{ JsError, JsSuccess, Json }
 import play.api.mvc.{ Action, BodyParsers, Controller }
 
 import scala.concurrent.ExecutionContext.Implicits.global
+import doobie.imports._
 import scala.concurrent.Future
+import scalaz.concurrent.Task
 import scalaz.{ -\/, \/, \/- }
 
 import com.fortysevendeg.exercises.services._
 import com.fortysevendeg.exercises.app._
 import com.fortysevendeg.shared.free.ExerciseOps
+import com.fortysevendeg.exercises.services.free.UserOps
 import com.fortysevendeg.exercises.services.interpreters.ProdInterpreters._
 
 class ExercisesController(
     implicit
-    exerciseOps:  ExerciseOps[ExercisesApp],
-    userServices: UserServices
+    exerciseOps: ExerciseOps[ExercisesApp],
+    userOps:     UserOps[ExercisesApp],
+    T:           Transactor[Task]
 ) extends Controller with JsonFormats {
 
   def evaluate(libraryName: String, sectionName: String) = Action(BodyParsers.parse.json) { request ⇒
