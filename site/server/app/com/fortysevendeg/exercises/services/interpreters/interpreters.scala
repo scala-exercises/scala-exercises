3983d49013f4d2cae1a90ce8700e504e37e92f69
raulraja <raulraja@gmail.com>
2016-02-05 16:09:33 +0100

    Added extensible interpreters for any F[_] including cats.Id

--- site/server/app/com/fortysevendeg/exercises/services/interpreters/interpreters.scala
+++ site/server/app/com/fortysevendeg/exercises/services/interpreters/interpreters.scala
@@ -1,62 +1,39 @@
 package com.fortysevendeg.exercises.services.interpreters
 
-import play.api.{ Play }
-import cats.data.Coproduct
-import doobie.imports._
-import cats.Id
-import cats.{ ~>, Monad }
+import cats._
 import cats.free.Free
-import scalaz.\/
 import scalaz.concurrent.Task
 import scala.language.higherKinds
 import com.fortysevendeg.shared.free._
-import com.fortysevendeg.exercises.app._
-import com.fortysevendeg.exercises.models.{ UserDoobieStore }
-import com.fortysevendeg.exercises.services.free._
+import com.fortysevendeg.exercises.services.free.UserOp
 
-class ProdInterpreters(implicit transactor: Transactor[Task]) {
-  val exerciseAndUserInterpreter: ExercisesAndUserOps ~> Task =
-    exerciseOpsInterpreter or userOpsInterpreter
-
-  val interpreters: ExercisesApp ~> Task =
-    dbOpsInterpreter or exerciseAndUserInterpreter
+/** Generic interpreters that can be lazily lifted via evidence of the target F via Applicative Pure Eval
+  */
+trait Interpreters[F[_]] extends InterpreterInstances {
 
   /** Lifts Exercise Ops to an effect capturing Monad such as Task via natural transformations
     */
-  def exerciseOpsInterpreter: ExerciseOp ~> Task = new (ExerciseOp ~> Task) {
+  implicit def exerciseOpsInterpreter(implicit A: Applicative[F]): ExerciseOp ~> F = new (ExerciseOp ~> F) {
 
     import com.fortysevendeg.exercises.services.ExercisesService._
 
-    def apply[A](fa: ExerciseOp[A]): Task[A] = fa match {
-      case GetLibraries()                       ⇒ Task.fork(Task.delay(libraries))
-      case GetSection(libraryName, sectionName) ⇒ Task.fork(Task.delay(section(libraryName, sectionName)))
-      case Evaluate(evalInfo)                   ⇒ Task.fork(Task.delay(evaluate(evalInfo)))
+    def apply[A](fa: ExerciseOp[A]): F[A] = fa match {
+      case GetLibraries()                       ⇒ A.pureEval(Eval.later(libraries))
+      case GetSection(libraryName, sectionName) ⇒ A.pureEval(Eval.later(section(libraryName, sectionName)))
+      case Evaluate(evalInfo)                   ⇒ A.pureEval(Eval.later(evaluate(evalInfo)))
     }
 
   }
 
-  def userOpsInterpreter: UserOp ~> Task = new (UserOp ~> Task) {
+  implicit def userOpsInterpreter(implicit A: Applicative[F]): UserOp ~> F = new (UserOp ~> F) {
 
-    def apply[A](fa: UserOp[A]): Task[A] = fa match {
-      case GetUsers()            ⇒ UserDoobieStore.all.transact(transactor)
-      case GetUserByLogin(login) ⇒ UserDoobieStore.getByLogin(login).transact(transactor)
-      case CreateUser(newUser)   ⇒ UserDoobieStore.create(newUser).transact(transactor)
-      case UpdateUser(user)      ⇒ UserDoobieStore.update(user).map(_.isDefined).transact(transactor)
-      case DeleteUser(user)      ⇒ UserDoobieStore.delete(user.id).transact(transactor)
-    }
+    def apply[A](fa: UserOp[A]): F[A] = ???
+
   }
 
-  def dbOpsInterpreter: DBResult ~> Task = new (DBResult ~> Task) {
-
-    def apply[A](fa: DBResult[A]): Task[A] = fa match {
-      case DBSuccess(value) ⇒ Task.now(value)
-      case DBFailure(error) ⇒ Task.fail(error)
-    }
-  }
 }
 
-object ProdInterpreters {
-  implicit def instance(implicit T: Transactor[Task]): ProdInterpreters = new ProdInterpreters
+trait InterpreterInstances {
 
   implicit val taskMonad: Monad[Task] = new Monad[Task] {
 
@@ -68,13 +45,16 @@ object ProdInterpreters {
     override def flatMap[A, B](fa: Task[A])(f: A ⇒ Task[B]): Task[B] =
       fa flatMap f
 
+    override def pureEval[A](x: Eval[A]): Task[A] =
+      Task.fork(Task.delay(x.value))
+
   }
 
-  implicit class FreeOps[A](f: Free[ExercisesApp, A]) {
-
-    /** Run this Free structure folding it's ops to an effect capturing task */
-    def runTask(implicit T: Transactor[Task]): Throwable \/ A = {
-      f.foldMap(instance.interpreters).attemptRun
-    }
-  }
 }
+
+/** Production based interpreters lifting ops to the effect capturing scalaz.concurrent.Task **/
+object ProdInterpreters extends Interpreters[Task]
+
+/** Test based interpreters lifting ops to their result identity **/
+object TestInterpreters extends Interpreters[Id]
+
