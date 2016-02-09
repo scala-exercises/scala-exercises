8953353d20cd6afc9d79e33aaf145e3610e7472a
Juan Pedro Moreno <juanpmorest@gmail.com>
2016-02-09 23:52:50 +0100

    SE-114 Adds Auth directive to enrich the request with the user id

---
+++ site/server/app/com/fortysevendeg/exercises/controllers/AuthenticationModule.scala
@@ -0,0 +1,26 @@
+package com.fortysevendeg.exercises.controllers
+
+import play.api.mvc.Results._
+import play.api.mvc._
+
+import scala.concurrent.Future
+
+trait AuthenticationModule {
+
+  case class UserRequest[A](val userId: String, request: Request[A]) extends WrappedRequest[A](request)
+
+  object AuthenticationAction extends ActionBuilder[UserRequest] {
+
+    override def invokeBlock[A](
+      request: Request[A],
+      block:   (UserRequest[A]) ⇒ Future[Result]
+    ): Future[Result] = {
+      request.session.get("user") match {
+        case Some(userId) ⇒ block(UserRequest(userId, request))
+        case None         ⇒ Future.successful(Unauthorized)
+      }
+    }
+  }
+}
+
+object AuthenticationModule extends AuthenticationModule
