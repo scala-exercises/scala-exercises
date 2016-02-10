772eccc81be720ed0d3a652db1eb648e1a8426a0
Rafa Paradela <rafa.p@47deg.com>
2016-02-10 12:18:33 +0100

    Fixed evolution issue

--- site/server/conf/evolutions/default/1.sql
+++ site/server/conf/evolutions/default/1.sql
@@ -1,13 +1,13 @@
 # --- !Ups
 
 CREATE TABLE "users" (
-    id serial primary key,
-    login varchar(255) unique not null,
-    name varchar(255) not null,
-    githubId varchar(255) not null,
-    pictureUrl varchar(255) not null,
-    githubUrl varchar(255) not null,
-    email varchar(255) not null
+    id bigserial primary key,
+    login text UNIQUE NOT NULL,
+    name text NOT NULL,
+    githubId text NOT NULL,
+    pictureUrl text NOT NULL,
+    githubUrl text NOT NULL,
+    email text NOT NULL    
 );
 
 # --- !Downs
