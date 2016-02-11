c0de1e8c17550ebdb22f8318515752faf88bf98e
Alejandro Gómez <alejandro@dialelo.com>
2016-02-11 17:08:36 +0100

    Add failing test for doc parser exception

---
+++ core/compiler/src/test/scala/com/fortysevendeg/exercises/compiler/DocParserSpec.scala
@@ -0,0 +1,17 @@
+package com.fortysevendeg.exercises
+package compiler
+
+import org.scalatest._
+
+class DocParserSpec extends FunSpec with Matchers {
+  describe("Exercise comments") {
+    it("can be parsed even when having trailing whitespace") {
+      val comment = """
+      /** The next line has a trailing whitespace
+        * 
+        */
+      """
+      assert(DocParser.parseLibraryDocComment(comment).isRight)
+    }
+  }
+}
