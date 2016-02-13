dc7b85bb695015eef52085b4b969d3fc7e1e2481
Alejandro Gómez <alejandro@dialelo.com>
2016-02-13 14:02:05 +0100

    Highlight Scala code correctly

--- core/compiler/src/main/scala/com/fortysevendeg/exercises/compiler/DocParser.scala
+++ core/compiler/src/main/scala/com/fortysevendeg/exercises/compiler/DocParser.scala
@@ -143,7 +143,7 @@ sealed trait DocRendering {
     case Paragraph(Chain(Summary(in) :: Nil)) if skipSummary ⇒ Nil
     case Paragraph(in)                                       ⇒ <p>{ inlineToHtml(in) }</p>
     case Code(data) ⇒
-      <pre>{ data }</pre>
+      <pre class={ "scala" }>{ data }</pre>
     case UnorderedList(items) ⇒
       <ul>{ listItemsToHtml(items) }</ul>
     case OrderedList(items, listStyle) ⇒
