fe468e927bc764e67f15e3bf8d914720b99d0945
Alejandro Gómez <alejandro@dialelo.com>
2016-02-13 11:39:57 +0100

    Highlight every preformatted code block

--- site/client/src/main/scala/utils/DomHandler.scala
+++ site/client/src/main/scala/utils/DomHandler.scala
@@ -39,7 +39,7 @@ object DomHandler {
   /** Highlights every preformatted code block.
     */
   def highlightCodeBlocks: IO[Unit] = io {
-    $("pre code").each((_: Any, code: dom.Element) ⇒ {
+    $("pre").each((_: Any, code: dom.Element) ⇒ {
       js.Dynamic.global.hljs.highlightBlock(code)
     })
   }
