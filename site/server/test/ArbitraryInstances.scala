d2766a506cd6df9c3ae20ee96555a6c26bed954f
Juan Pedro Moreno <juanpmorest@gmail.com>
2016-02-15 18:53:23 -0500

    More test cases and minor improvements

--- site/server/test/ArbitraryInstances.scala
+++ site/server/test/ArbitraryInstances.scala
@@ -1,40 +1,61 @@
 import cats.data.Xor
-import com.fortysevendeg.exercises.models.{UserDoobieStore, UserCreation}
 import com.fortysevendeg.exercises.models.UserCreation.Request
+import com.fortysevendeg.exercises.models.{ UserCreation, UserDoobieStore }
 import com.fortysevendeg.exercises.persistence.domain.SaveUserProgress
 import doobie.imports._
-import org.scalacheck.Arbitrary
+import org.scalacheck.{ Gen, Arbitrary }
+import org.scalacheck.Arbitrary.arbitrary
+import org.scalacheck.Shapeless._
 import org.scalatest.Assertions
-import shapeless.tag.@@
-import shared.{User, UserProgress}
-import org.scalacheck.Arbitrary._
+import shared.User
 
 import scalaz.concurrent.Task
 
 trait ArbitraryInstances extends Assertions {
 
-  type Id = Long
+  // Avoids creating strings with null chars because Postgres text fields don't support it.
+  // see http://stackoverflow.com/questions/1347646/postgres-error-on-insert-error-invalid-byte-sequence-for-encoding-utf8-0x0
+  implicit val stringArbitrary: Arbitrary[String] =
+    Arbitrary(Gen.identifier.map(_.replaceAll("\u0000", "")))
 
-  implicit val userProgressArbitrary : Arbitrary[Id]= {
-    Arbitrary(arbitrary[UserProgress].map(u => save(u).id))
-  }
+  implicit val userSaveRequestArbitrary: Arbitrary[Request] =
+    Arbitrary(for {
+      login ← Gen.uuid
+      name ← Gen.alphaStr
+      githubId ← Gen.uuid
+      githubUrl ← Gen.alphaStr
+      pictureUrl ← Gen.alphaStr
+      email ← Gen.alphaStr
+    } yield Request(
+      login = login.toString,
+      name = name,
+      githubId = githubId.toString,
+      pictureUrl = pictureUrl,
+      githubUrl = githubUrl,
+      email = email
+    ))
 
-
-
-  implicit def persistentUserArbitrary(implicit transactor: Transactor[Task]): Arbitrary[User] = {
+  def persistentUserArbitrary(implicit transactor: Transactor[Task]): Arbitrary[User] = {
     import UserCreation._
-    Arbitrary(arbitrary[Request] map { request =>
+    Arbitrary(arbitrary[Request] map { request ⇒
       UserDoobieStore.create(request).transact(transactor).run match {
-        case Xor.Right(user) => user
-        case Xor.Left(error) => fail("Failed generating persistent users : $error")
+        case Xor.Right(user) ⇒ user
+        case Xor.Left(error) ⇒ fail("Failed generating persistent users : $error")
       }
     })
   }
 
-  implicit def saveUserProgressArbitrary : Arbitrary[SaveUserProgress.Request] = {
+  case class UserProgressPair(request: SaveUserProgress.Request, user: User)
+
+  implicit def saveUserProgressArbitrary(implicit transactor: Transactor[Task]): Arbitrary[UserProgressPair] = {
+
     Arbitrary(for {
-      user <- arbitrary[User]
-      request <- arbitrary[SaveUserProgress.Request] map (p => p.copy(userId = user.id))
-    } yield request)
+      user ← persistentUserArbitrary.arbitrary
+      request ← {
+        arbitrary[SaveUserProgress.Request] map (p ⇒ p.copy(userId = user.id))
+      }
+    } yield UserProgressPair(request, user))
   }
 }
+
+object ArbitraryInstances extends ArbitraryInstances
--- site/server/test/UserProgressRepositorySpec.scala
+++ site/server/test/UserProgressRepositorySpec.scala
@@ -1,13 +1,9 @@
-import com.fortysevendeg.exercises.models.{ UserCreation, UserDoobieStore }
-import com.fortysevendeg.exercises.persistence.domain.SaveUserProgress
-import com.fortysevendeg.exercises.persistence.domain.SaveUserProgress._
 import com.fortysevendeg.exercises.persistence.repositories.UserProgressDoobieRepository.{ instance ⇒ repository }
 import doobie.imports._
-import org.scalacheck.{ Arbitrary, Gen }
 import org.scalatest._
-import org.scalacheck.Shapeless._
 import org.scalatest.prop.GeneratorDrivenPropertyChecks
 import test.database.TestDatabase
+import org.scalacheck.Shapeless._
 
 import scalaz.concurrent.Task
 
@@ -15,133 +11,95 @@ class UserProgressRepositorySpec
     extends PropSpec
     with GeneratorDrivenPropertyChecks
     with Matchers
-    with BeforeAndAfterEach {
-
-  // User Progress Creation generator
-  implicitly[Arbitrary[SaveUserProgress.Request]]
-
-  // Avoids creating strings with null chars because Postgres text fields don't support it.
-  // http://stackoverflow.com/questions/1347646/postgres-error-on-insert-error-invalid-byte-sequence-for-encoding-utf8-0x0
-  implicit val stringArbitrary: Arbitrary[String] = Arbitrary(Gen.identifier.map(_.replaceAll("\u0000", "")))
+    with ArbitraryInstances {
 
   val db = TestDatabase.create
   TestDatabase.update(db)
 
-  val transactor: Transactor[Task] = TestDatabase.transactor(db)
-
-  import transactor.yolo._
-
-  //Satisfy User Foreign key:
-  val userId = UserDoobieStore.create(new UserCreation.Request(
-    login = "test" + System.currentTimeMillis(),
-    name = "Test",
-    githubId = "1",
-    pictureUrl = "",
-    githubUrl = "",
-    email = "test@test.com"
-  )).transact(transactor).run.toOption map (_.id) getOrElse 0l
-
-  val userProgressGen = for {
-    userProgressRequest ← Arbitrary.arbitrary[SaveUserProgress.Request]
-    libraryName ← Gen.identifier
-    sectionName ← Gen.identifier
-    method ← Gen.identifier
-    version ← Gen.choose(1, 100)
-    exerciseType ← Gen.oneOf(Other, Koans)
-    args ← Gen.option(stringArbitrary.arbitrary)
-    succeeded ← Gen.oneOf(true, false)
-  } yield userProgressRequest.copy(
-    userId = userId,
-    libraryName = libraryName,
-    sectionName = sectionName,
-    method = method,
-    version = version,
-    exerciseType = exerciseType,
-    args = args,
-    succeeded = succeeded
-  )
-
-  override def beforeEach() = repository.deleteAll().quick.run
+  implicit val transactor: Transactor[Task] = TestDatabase.transactor(db)
 
   property("new user progress records can be created") {
-    forAll(userProgressGen) { newUserProgress ⇒
-      val userProgress = repository.create(newUserProgress).transact(transactor).run
+    forAll { pair: UserProgressPair ⇒
+      val userProgress = repository.create(pair.request).transact(transactor).run
 
-      userProgress shouldBe newUserProgress.asUserProgress(userProgress.id)
+      userProgress shouldBe pair.request.asUserProgress(userProgress.id)
     }
   }
 
   property("user progress can be fetched by section and exercise") {
-    forAll(userProgressGen) { newUserProgress ⇒
-      repository.create(newUserProgress).quick.run
+    forAll { pair: UserProgressPair ⇒
+      repository.create(pair.request).transact(transactor).run
 
       val currentUserProgress =
         repository.findByExerciseVersion(
-          userId = newUserProgress.userId,
-          libraryName = newUserProgress.libraryName,
-          sectionName = newUserProgress.sectionName,
-          method = newUserProgress.method,
-          version = newUserProgress.version
+          userId = pair.user.id,
+          libraryName = pair.request.libraryName,
+          sectionName = pair.request.sectionName,
+          method = pair.request.method,
+          version = pair.request.version
         ).transact(transactor).run
       currentUserProgress.foreach(up ⇒ {
-        up shouldBe newUserProgress.asUserProgress(up.id)
+        up shouldBe pair.request.asUserProgress(up.id)
       })
     }
   }
 
   property("user progress can be deleted") {
-    forAll(userProgressGen) { newUserProgress ⇒
-      val userProgress = repository.create(newUserProgress).transact(transactor).run
+    forAll { pair: UserProgressPair ⇒
 
-      repository.delete(userProgress.id).quick.run
+      val userProgress = repository.create(pair.request).transact(transactor).run
+
+      repository.delete(userProgress.id).transact(transactor).run
       repository.findById(userProgress.id).transact(transactor).run shouldBe empty
     }
   }
 
   property("all user progress records can be deleted") {
-    forAll(userProgressGen) { newUserProgress ⇒
-      val userProgress = repository.create(newUserProgress).transact(transactor).run
+    forAll { pair: UserProgressPair ⇒
+      val userProgress = repository.create(pair.request).transact(transactor).run
 
-      repository.deleteAll().quick.run
+      repository.deleteAll().transact(transactor).run
       repository.findById(userProgress.id).transact(transactor).run shouldBe empty
     }
   }
 
   property("users progress can be queried by their ID") {
-    forAll(userProgressGen) { newUserProgress ⇒
-      val userProgress = repository.create(newUserProgress).transact(transactor).run
+    forAll { pair: UserProgressPair ⇒
+      val userProgress = repository.create(pair.request).transact(transactor).run
 
       val maybeUserProgress = repository.findById(userProgress.id).transact(transactor).run
       maybeUserProgress.fold(false)(up ⇒ {
-        up == newUserProgress.asUserProgress(up.id)
+        up == pair.request.asUserProgress(up.id)
       }) shouldBe true
     }
   }
 
   property("users progress List can be queried by the user ID") {
-    forAll(userProgressGen) { newUserProgress ⇒
-      val userProgress = repository.create(newUserProgress).transact(transactor).run
+    forAll { pair: UserProgressPair ⇒
+      val userProgress = repository.create(pair.request).transact(transactor).run
 
       val userProgressList = repository.findByUserId(userProgress.userId).transact(transactor).run
 
       userProgressList should not be empty
       userProgressList foreach { up ⇒
-        up shouldBe newUserProgress.asUserProgress(up.id)
+        up shouldBe pair.request.asUserProgress(up.id)
       }
     }
   }
 
   property("users progress can be updated") {
-    forAll(userProgressGen) { newUserProgress ⇒
-      val userProgress = repository.create(newUserProgress).transact(transactor).run
-      val storedUserProgress = repository.findById(userProgress.id).transact(transactor).run
+    forAll { pair: UserProgressPair ⇒
 
-      storedUserProgress.fold(false)(u ⇒ {
-        val modifiedUser = u.copy(version = u.version + 1)
-        repository.update(userProgress = modifiedUser, newSucceededValue = true).quick.run
+      val userProgress = repository.create(pair.request).transact(transactor).run
+      val modifiedUser = userProgress.copy(version = userProgress.version + 1)
+      repository.update(userProgress = modifiedUser, newSucceededValue = !userProgress.succeeded).transact(transactor).run
+      val fetchedProgress = repository.findById(userProgress.id).transact(transactor).run
 
-        repository.findById(u.id).transact(transactor).run.contains(modifiedUser)
-      }) shouldBe true
+      fetchedProgress foreach { up ⇒
+        up.succeeded shouldBe !userProgress.succeeded
+        up.id shouldBe userProgress.id
+        up.userId shouldBe userProgress.userId
+      }
     }
   }
 }
