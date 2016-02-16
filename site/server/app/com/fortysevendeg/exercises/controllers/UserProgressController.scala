c00353e5a47eafc282928d31ede14cf257f1380e
Juan Pedro Moreno <juanpmorest@gmail.com>
2016-02-16 16:41:34 -0500

    Adds the new User Progress controller

---
+++ site/server/app/com/fortysevendeg/exercises/controllers/UserProgressController.scala
@@ -0,0 +1,38 @@
+/*
+ * scala-exercises-server
+ * Copyright (C) 2015-2016 47 Degrees, LLC. <http://www.47deg.com>
+ */
+
+package com.fortysevendeg.exercises.controllers
+
+import com.fortysevendeg.exercises.app._
+import com.fortysevendeg.exercises.services.free.{ UserOps, UserProgressOps }
+import com.fortysevendeg.shared.free.ExerciseOps
+import doobie.imports._
+import play.api.mvc.Controller
+
+import scalaz.concurrent.Task
+
+class UserProgressController(
+    implicit
+    exerciseOps:     ExerciseOps[ExercisesApp],
+    userOps:         UserOps[ExercisesApp],
+    userProgressOps: UserProgressOps[ExercisesApp],
+    T:               Transactor[Task]
+) extends Controller with JsonFormats with AuthenticationModule {
+
+  def fetchUserProgress() =
+    AuthenticatedUser {
+      user ⇒ ???
+    }
+
+  def fetchUserProgressByLibrary(libraryName: String) =
+    AuthenticatedUser {
+      user ⇒ ???
+    }
+
+  def fetchUserProgressBySection(libraryName: String, sectionName: String) =
+    AuthenticatedUser {
+      user ⇒ ???
+    }
+}
