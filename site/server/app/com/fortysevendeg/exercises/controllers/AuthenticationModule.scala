eb0861a23af0b7095bdf115d2509535db6ce41a8
Juan Pedro Moreno <juanpmorest@gmail.com>
2016-02-16 16:41:05 -0500

    Provides new auth directives and some improvements about the authentication and the controllers

--- site/server/app/com/fortysevendeg/exercises/controllers/AuthenticationModule.scala
+++ site/server/app/com/fortysevendeg/exercises/controllers/AuthenticationModule.scala
@@ -5,10 +5,18 @@
 
 package com.fortysevendeg.exercises.controllers
 
+import cats.data.Xor
+import com.fortysevendeg.exercises.app._
+import com.fortysevendeg.exercises.services.free.UserOps
+import com.fortysevendeg.exercises.services.interpreters.ProdInterpreters._
+import doobie.imports._
+import play.api.libs.json._
 import play.api.mvc.Results._
 import play.api.mvc._
+import shared.User
 
 import scala.concurrent.Future
+import scalaz.concurrent._
 
 trait AuthenticationModule {
 
@@ -19,13 +27,33 @@ trait AuthenticationModule {
     override def invokeBlock[A](
       request: Request[A],
       block:   (UserRequest[A]) ⇒ Future[Result]
-    ): Future[Result] = {
+    ): Future[Result] =
       request.session.get("user") match {
         case Some(userId) ⇒ block(UserRequest(userId, request))
-        case None         ⇒ Future.successful(Unauthorized)
+        case None         ⇒ Future.successful(Forbidden)
+      }
+  }
+
+  def AuthenticatedUser(block: User ⇒ Result)(implicit userOps: UserOps[ExercisesApp], transactor: Transactor[Task]) =
+    AuthenticationAction { request ⇒
+      userOps.getUserByLogin(request.userId).runTask match {
+        case Xor.Right(Some(user)) ⇒ block(user)
+        case _                     ⇒ BadRequest("User login not found")
       }
     }
-  }
+
+  def AuthenticatedUser[T](bodyParser: BodyParser[JsValue])(block: (T, User) ⇒ Result)(implicit userOps: UserOps[ExercisesApp], transactor: Transactor[Task], format: Reads[T]) =
+    AuthenticationAction(bodyParser) { request ⇒
+      request.body.validate[T] match {
+        case JsSuccess(validatedBody, _) ⇒
+          userOps.getUserByLogin(request.userId).runTask match {
+            case Xor.Right(Some(user)) ⇒ block(validatedBody, user)
+            case _                     ⇒ BadRequest("User login not found")
+          }
+        case JsError(errors) ⇒
+          BadRequest(JsError.toJson(errors))
+      }
+    }
 }
 
 object AuthenticationModule extends AuthenticationModule
