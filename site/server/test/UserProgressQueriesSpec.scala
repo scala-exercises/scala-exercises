8d06fbd831a35ea674adc01c25e838e7cb2bd950
Juan Pedro Moreno <juanpmorest@gmail.com>
2016-02-15 20:28:55 -0500

    Adds a Database Instance to be used in the test specs

--- site/server/test/UserProgressQueriesSpec.scala
+++ site/server/test/UserProgressQueriesSpec.scala
@@ -1,18 +1,11 @@
 import doobie.contrib.specs2.analysisspec.AnalysisSpec
-import doobie.imports._
 import doobie.util.query.Query
 import org.specs2.mutable.Specification
 import shared.UserProgress
-import test.database.TestDatabase
 import com.fortysevendeg.exercises.persistence.domain.{ UserProgressQueries ⇒ Q }
+import test.database.DatabaseInstance
 
-import scalaz.concurrent.Task
-
-class UserProgressQueriesSpec extends Specification with AnalysisSpec {
-  val db = TestDatabase.create
-  TestDatabase.update(db)
-
-  val transactor: Transactor[Task] = TestDatabase.transactor(db)
+class UserProgressQueriesSpec extends Specification with AnalysisSpec with DatabaseInstance {
 
   check(Query[Unit, UserProgress](Q.all))
   check(Query[Long, UserProgress](Q.findById))
