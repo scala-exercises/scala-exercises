d3fa43b3a5421959d2e663e180c4fc050a191d01
Juan Pedro Moreno <juanpmorest@gmail.com>
2016-02-17 10:26:43 -0500

    Provides the endpoint implementation

--- site/server/app/com/fortysevendeg/exercises/controllers/UserProgressController.scala
+++ site/server/app/com/fortysevendeg/exercises/controllers/UserProgressController.scala
@@ -5,10 +5,13 @@
 
 package com.fortysevendeg.exercises.controllers
 
+import cats.data.Xor
 import com.fortysevendeg.exercises.app._
 import com.fortysevendeg.exercises.services.free.{ UserOps, UserProgressOps }
 import com.fortysevendeg.shared.free.ExerciseOps
+import com.fortysevendeg.exercises.services.interpreters.ProdInterpreters._
 import doobie.imports._
+import play.api.libs.json.Json
 import play.api.mvc.Controller
 
 import scalaz.concurrent.Task
@@ -22,7 +25,10 @@ class UserProgressController(
 ) extends Controller with JsonFormats with AuthenticationModule {
 
   def fetchUserProgressBySection(libraryName: String, sectionName: String) =
-    AuthenticatedUser {
-      user ⇒ ???
+    AuthenticatedUser { user ⇒
+      userProgressOps.fetchUserProgressByLibrarySection(user, libraryName, sectionName).runTask match {
+        case Xor.Right(response) ⇒ Ok(Json.toJson(response))
+        case Xor.Left(error)     ⇒ BadRequest(error.getMessage)
+      }
     }
 }
