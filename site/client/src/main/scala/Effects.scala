781474a3711f6f27147a3f7c27d6b4973be88eca
Alejandro Gómez <alejandro@dialelo.com>
2016-02-18 20:24:46 +0100

    Make the Start action a case object

--- site/client/src/main/scala/Effects.scala
+++ site/client/src/main/scala/Effects.scala
@@ -18,7 +18,7 @@ object Effects {
   def noop: Future[Option[Action]] = Future(None)
 
   def perform(s: State, a: Action): Future[Option[Action]] = a match {
-    case Start()                 ⇒ loadInitialData
+    case Start                   ⇒ loadInitialData
     case CompileExercise(method) ⇒ compileExercise(s, method)
     case _                       ⇒ noop
   }
