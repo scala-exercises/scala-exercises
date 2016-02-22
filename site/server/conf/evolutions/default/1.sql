e07a6c68578d0382d9e1f28c39905d70775a6d5a
Juan Pedro Moreno <juanpmorest@gmail.com>
2016-02-22 22:34:13 +0100

    SE-222 Squeezes the sql evolutions into a single file

--- site/server/conf/evolutions/default/1.sql
+++ site/server/conf/evolutions/default/1.sql
@@ -10,6 +10,19 @@ CREATE TABLE "users" (
     email varchar(255)
 );
 
+CREATE TABLE "userProgress" (
+    id bigserial primary key,
+    userId bigint not null,
+    libraryName varchar(255) not null,
+    sectionName varchar(255) not null,
+    method varchar(255) not null,
+    version int not null,
+    exerciseType varchar(255) not null,
+    args text[],
+    succeeded boolean not null default false,
+FOREIGN KEY (userId) REFERENCES users (id));
+
 # --- !Downs
 
+DROP TABLE "userProgress";
 DROP TABLE "users";
