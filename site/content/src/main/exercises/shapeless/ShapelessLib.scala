b2b2b7a082b07a0149c302a931d0a764e28b73c4
Raúl Raja Martínez <raulraja@gmail.com>
2016-02-24 10:06:55 +0100

    Updated shapeless desc

--- site/content/src/main/exercises/shapeless/ShapelessLib.scala
+++ site/content/src/main/exercises/shapeless/ShapelessLib.scala
@@ -2,7 +2,7 @@ package shapelessex
 
 /** shapeless
   *
-  * Exercises for shapeless
+  * shapeless is a type class and dependent type based generic programming library for Scala
   */
 object ShapelessLib extends exercise.Library {
   override def color = Some("#6573C4")
