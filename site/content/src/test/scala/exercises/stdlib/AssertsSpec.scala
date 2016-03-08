cdad7e87423bd57b07c691d4dd2242130740c3dd
Alejandro Gómez <alejandro@dialelo.com>
2016-03-08 15:34:16 +0100

    Almost complete tests of case classes exercises

--- site/content/src/test/scala/exercises/stdlib/AssertsSpec.scala
+++ site/content/src/test/scala/exercises/stdlib/AssertsSpec.scala
@@ -57,3 +57,102 @@ class ByNameParameterSpec extends Spec with Checkers {
     )
   }
 }
+
+class CaseClassesSpec extends Spec with Checkers {
+  // FIXME: get rid of this
+  import org.scalacheck.Shapeless._
+
+  def `case classes comparisons` = {
+    check(
+      Test.success(
+        CaseClasses.caseClassesSupportEquality _,
+        false :: true :: false :: false :: HNil
+      )
+    )
+  }
+
+  def `hash codes` = {
+    check(
+      Test.success(
+        CaseClasses.hashcodeMethodCaseClasses _,
+        false :: true :: HNil
+      )
+    )
+  }
+
+  def `case class creation` = {
+    check(
+      Test.success(
+        CaseClasses.creationCaseClasses _,
+        true :: false :: false :: HNil
+      )
+    )
+  }
+
+  def `to string method` = {
+    check(
+      Test.success(
+        CaseClasses.toStringMethodCaseClasses _,
+        "Dog(Scooby,Doberman)" :: HNil
+      )
+    )
+  }
+
+  def `properties` = {
+    check(
+      Test.success(
+        CaseClasses.propertiesCaseClasses _,
+        "Scooby" :: "Doberman" :: HNil
+      )
+    )
+  }
+
+  def `mutable properties` = {
+    check(
+      Test.success(
+        CaseClasses.mutablePropertiesCaseClasses _,
+        "Scooby" :: "Doberman" :: "Scooby Doo" :: "Doberman" :: HNil
+      )
+    )
+  }
+
+  def `altering case classes` = {
+    check(
+      Test.success(
+        CaseClasses.alteringCaseClasses _,
+        "Scooby" :: "Doberman" :: "Scooby Doo" :: "Doberman" :: HNil
+      )
+    )
+  }
+
+  // FIXME: Shapeless doesn't find an implicit FnToProduct.Aux, why?
+  // def `case class parameters` = {
+  //   check(
+  //     Test.success(
+  //       CaseClasses.parametersCaseClasses _,
+  //       "Fred" :: "Jones" :: 23 :: "111-22-3333" ::
+  //         "Samantha" :: "Jones" :: 0 :: "111-22-3333" ::
+  //         "Fred" :: "Jones" :: 0 :: "111-22-3333" ::
+  //         "Fred" :: "Jones" :: 23 :: "111-22-3333" :: HNil
+  //     )
+  //   )
+  // }
+
+  def `case classes as tuples` = {
+    check(
+      Test.success(
+        CaseClasses.asTupleCaseClasses _,
+        "Fred" :: "Jones" :: 23 :: "111-22-3333" :: HNil
+      )
+    )
+  }
+
+  def `case classes are serializable` = {
+    check(
+      Test.success(
+        CaseClasses.serializableCaseClasses _,
+        true :: false :: HNil
+      )
+    )
+  }
+}
