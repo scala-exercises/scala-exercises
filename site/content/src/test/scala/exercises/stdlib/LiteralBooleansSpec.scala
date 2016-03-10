0b9adc70f6d9b88a0a6288331ed53373ac8811c4
Alejandro Gómez <alejandro@dialelo.com>
2016-03-10 11:57:08 +0100

    Test boolean literals exercises

---
+++ site/content/src/test/scala/exercises/stdlib/LiteralBooleansSpec.scala
@@ -0,0 +1,21 @@
+package exercises
+
+import stdlib._
+import shapeless.HNil
+
+import org.scalatest.Spec
+import org.scalatest.prop.Checkers
+
+// FIXME: get rid of this if possible
+import org.scalacheck.Shapeless._
+
+class LiteralBooleansSpec extends Spec with Checkers {
+  def `are either true or false` = {
+    check(
+      Test.success(
+        LiteralBooleans.literalBooleanLiteralBooleans _,
+        true :: false :: false :: true :: false :: false :: HNil
+      )
+    )
+  }
+}
