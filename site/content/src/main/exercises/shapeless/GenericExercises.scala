0a1a2f99b6fe4493c8f47688f41a96b1064272be
raulraja <raulraja@gmail.com>
2016-03-21 16:54:42 +0100

    Progress toward `Generic Exercises` Labelled Checkpoint

--- site/content/src/main/exercises/shapeless/GenericExercises.scala
+++ site/content/src/main/exercises/shapeless/GenericExercises.scala
@@ -19,6 +19,18 @@ object GenericExercises extends FlatSpec with Matchers with exercise.Section {
     val fooGen = Generic[Foo]
 
     val foo = Foo(23, "foo", true)
+
+    import poly._
+
+    // Simple recursive case class family
+    sealed trait Tree[T]
+    case class Leaf[T](t: T) extends Tree[T]
+    case class Node[T](left: Tree[T], right: Tree[T]) extends Tree[T]
+
+    // Polymorphic function which adds 1 to any Int and is the identity
+    // on all other values
+    object inc extends ->((i: Int) => i+1)
+
   }
 
   import Helper._
@@ -39,6 +51,54 @@ object GenericExercises extends FlatSpec with Matchers with exercise.Section {
     val r = 13 :: l.tail
     val newFoo = fooGen.from(r)
     newFoo.i should be (res1)
+  }
+
+  /** Typically values of Generic for a given case class are materialized using an implicit macro, 
+    * allowing a wide variety of structural programming problems to be solved with no or minimal boilerplate. 
+    * In particular the existing lens, Scrap Your Boilerplate and generic zipper implementations are now available 
+    * for any case class family (recursive families included, as illustrated below) without any additional boilerplate being required
+    * {{{
+    * import poly._
+    * 
+    * // Simple recursive case class family
+    * sealed trait Tree[T]
+    * case class Leaf[T](t: T) extends Tree[T]
+    * case class Node[T](left: Tree[T], right: Tree[T]) extends Tree[T]
+    * 
+    * // Polymorphic function which adds 1 to any Int and is the identity
+    * // on all other values
+    * object inc extends ->((i: Int) => i+1)
+    * }}}
+    */
+  def structural(res0 : Int, res1 : Int, res2 : Int) = {
+    
+    val tree: Tree[Int] =
+          Node(
+            Leaf(1),
+            Node(
+              Leaf(2),
+              Leaf(3)))
+
+    // Transform tree by applying inc everywhere
+    everywhere(inc)(tree) should be (
+      Node(
+            Leaf(res0),
+            Node(
+              Leaf(res1),
+              Leaf(res2)))
+    )
+  }
+
+  /** A natural extension of Generic's mapping of the content of data types onto a sum of products representation 
+    * is to a mapping of the data type including its constructor and field names onto a labelled sum of products representation, 
+    * ie. a representation in terms of the discriminated unions and records that we saw above. 
+    * This is provided by LabelledGeneric. Currently it provides the underpinnings for the use of shapeless lenses with 
+    * symbolic path selectors (see next section) and it is expected that it will support many scenarios which would otherwise 
+    * require the support of hard to maintain special case macros.
+    */
+  def labelled() = {
+
+
   }
 
 }
