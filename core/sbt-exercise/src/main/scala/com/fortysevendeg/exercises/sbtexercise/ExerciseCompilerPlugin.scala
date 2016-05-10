5945dd422bfc7a7a2c67ecc818e1abe610e52873
Rafa Paradela <rafa.p@47deg.com>
2016-05-10 17:01:19 +0200

    Avoids exception if splitter doesn't match

--- core/sbt-exercise/src/main/scala/com/fortysevendeg/exercises/sbtexercise/ExerciseCompilerPlugin.scala
+++ core/sbt-exercise/src/main/scala/com/fortysevendeg/exercises/sbtexercise/ExerciseCompilerPlugin.scala
@@ -194,7 +194,7 @@ object ExerciseCompilerPlugin extends AutoPlugin {
         }
       }
 
-    def relativePath(absolutePath: String, splitter: String) = splitter + absolutePath.split(splitter)(1)
+    private[this] def relativePath(absolutePath: String, splitter: String) = splitter + absolutePath.split(splitter).lift(1).getOrElse("")
 
     val result = for {
       compilerClass ← catching(loader.loadClass(COMPILER_CLASS))(
