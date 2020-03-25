48e0b389568ec3f997426ab341bdc4861a1ec40e
franciscodr <francisco.d@47deg.com>
2020-03-25 21:28:50 +0100

    Bumps up github4s version

--- core/shared/src/main/scala/org/scalaexercises/types/progress.scala
+++ core/shared/src/main/scala/org/scalaexercises/types/progress.scala
@@ -23,40 +23,49 @@ import org.scalaexercises.types.user._
 import org.scalaexercises.types.exercises._
 
 case class UserProgress(
-  id:           Long,
-  userId:       Long,
-  libraryName:  String,
-  sectionName:  String,
-  method:       String,
-  version:      Int,
-  exerciseType: ExerciseType = Other,
-  args:         List[String],
-  succeeded:    Boolean
+    id: Long,
+    userId: Long,
+    libraryName: String,
+    sectionName: String,
+    method: String,
+    version: Int,
+    exerciseType: ExerciseType = Other,
+    args: List[String],
+    succeeded: Boolean
 )
 
 object SaveUserProgress {
   case class Request(
-      user:         User,
-      libraryName:  String,
-      sectionName:  String,
-      method:       String,
-      version:      Int,
+      user: User,
+      libraryName: String,
+      sectionName: String,
+      method: String,
+      version: Int,
       exerciseType: ExerciseType,
-      args:         List[String],
-      succeeded:    Boolean
+      args: List[String],
+      succeeded: Boolean
   ) {
 
     def asUserProgress(id: Long): UserProgress =
-      UserProgress(id, user.id, libraryName, sectionName, method, version, exerciseType, args, succeeded)
+      UserProgress(
+        id,
+        user.id,
+        libraryName,
+        sectionName,
+        method,
+        version,
+        exerciseType,
+        args,
+        succeeded)
   }
 }
 
 // Overall progress
 
 case class OverallUserProgressItem(
-    libraryName:       String,
+    libraryName: String,
     completedSections: Int,
-    totalSections:     Int
+    totalSections: Int
 ) {
   def completed: Boolean = completedSections == totalSections
 }
@@ -68,9 +77,9 @@ case class OverallUserProgress(libraries: List[OverallUserProgressItem])
 case class ExerciseProgress(methodName: String, args: List[String], succeeded: Boolean)
 
 case class SectionExercises(
-    libraryName:    String,
-    sectionName:    String,
-    exercises:      List[ExerciseProgress],
+    libraryName: String,
+    sectionName: String,
+    exercises: List[ExerciseProgress],
     totalExercises: Int
 ) {
   def sectionSucceeded = exercises.filter(_.succeeded).size == totalExercises
@@ -79,14 +88,14 @@ case class SectionExercises(
 // Per-section and library progress
 
 case class SectionProgress(
-  sectionName: String,
-  succeeded:   Boolean
+    sectionName: String,
+    succeeded: Boolean
 )
 
 case class LibraryProgress(
     libraryName: String,
-    sections:    List[SectionProgress]
+    sections: List[SectionProgress]
 ) {
-  def totalSections: Int = sections.size
+  def totalSections: Int     = sections.size
   def completedSections: Int = sections.filter(_.succeeded).size
 }
