c7f4e44948ac52a714e82b29420101020580f93b
Juan Pedro Moreno <juanpmorest@gmail.com>
2020-04-23 17:57:59 +0200

    Using latest snapshot. Final versions don't work 🤔

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -32,7 +32,7 @@ object ProjectPlugin extends AutoPlugin {
         "circe"         -> "0.6.0-SNAPSHOT",
         "fetch"         -> "0.6.0-SNAPSHOT",
         "monocle"       -> "0.6.0-SNAPSHOT",
-        "pureconfig"    -> "0.1.2"
+        "pureconfig"    -> "0.1.2+1-5289403e-SNAPSHOT"
       ) ++ Map(
         // JVM Versions
         "catsversion"         -> "2.1.1",
