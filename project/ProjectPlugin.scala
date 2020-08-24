52b415260d89a52928bd2545dffb2c71f1714090
47erbot <developer@47deg.com>
2020-08-24 07:13:02 +0200

    Update scalatest to 3.2.2

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -42,7 +42,7 @@ object ProjectPlugin extends AutoPlugin {
       val scalacheck: String                = "1.14.3"
       val scalacheckShapeless: String       = "1.2.5"
       val scalajsscripts: String            = "1.1.4"
-      val scalatest: String                 = "3.2.1"
+      val scalatest: String                 = "3.2.2"
       val scalatestplusScheck: String       = "3.2.1.0"
       val shapeless: String                 = "2.3.3"
       val testcontainers: String            = "0.38.1"
