49dd0cab251517e3c9c1502d1e28b90463bdc7e2
47erbot <developer@47deg.com>
2020-09-09 05:10:48 +0000

    Update cats-effect to 2.2.0

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -14,7 +14,7 @@ object ProjectPlugin extends AutoPlugin {
     lazy val V = new {
       val bootstrap: String                 = "3.3.7"
       val cats: String                      = "2.2.0"
-      val catsEffect: String                = "2.1.4"
+      val catsEffect: String                = "2.2.0"
       val commonsio: String                 = "2.7"
       val doobie: String                    = "0.9.0"
       val exercisesCats: String             = "0.6.4"
