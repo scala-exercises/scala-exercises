69b9a996abd96dca2ec7aae3228afb54a0d5ae04
47erbot <developer@47deg.com>
2021-05-26 06:01:49 +0000

    Update testcontainers-scala-postgresql, ... to 0.39.5

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -45,7 +45,7 @@ object ProjectPlugin extends AutoPlugin {
       val scalatest: String                 = "3.2.8"
       val scalatestplusScheck: String       = "3.2.2.0"
       val shapeless: String                 = "2.3.3"
-      val testcontainers: String            = "0.39.4"
+      val testcontainers: String            = "0.39.5"
       val upickle: String                   = "1.3.15"
       val webjars: String                   = "2.8.0-1"
     }
