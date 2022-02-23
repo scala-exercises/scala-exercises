be78a34b78ec8d77d13cf2d033525be965610b94
47erbot <developer@47deg.com>
2022-02-23 06:42:26 +0100

    Update utest to 0.7.11

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -33,7 +33,7 @@ object ProjectPlugin extends AutoPlugin {
       val jsDependencyScalajsdom: String    = "1.2.0"
       val jsDependencyScalajsjquery: String = "1.0.0"
       val jsDependencyScalatags: String     = "0.9.4"
-      val jsDependencyUtest: String         = "0.7.10"
+      val jsDependencyUtest: String         = "0.7.11"
       val jqueryWebjar: String              = "3.6.0"
       val knockoff: String                  = "0.8.14"
       val monix: String                     = "3.4.0"
